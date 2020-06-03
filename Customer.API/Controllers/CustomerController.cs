e0805a6194e6da584a4fbf160ce603576e15aea7
Tiffanatic <madsrynord@live.dk>
2020-06-03 13:12:09 +0200

    Code Cleanup

---
+++ Customer.API/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        [HttpGet]
+        public String Get()
+        {
+            return "Customer API is running";
+        }
+    }
+}
